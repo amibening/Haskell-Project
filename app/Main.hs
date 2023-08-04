68d37f38dd0f2f9cde084fa7e8cfefdfa9462192
amibening <amibening@hotmail.com>
2023-08-04 12:51:16 +0000

    change to myiAddressBook

--- app/Main.hs
+++ app/Main.hs
@@ -3,25 +3,25 @@ import Lib
 main :: IO ()
 main = do
   putStrLn "Welcome to My International Address Book!"
-  addrs <- myAddressBook []
+  addrs <- myiAddressBook []
   printAddresses addrs
   where
-    myAddressBook :: [Address] -> IO [Address]
-    myAddressBook addrs = do
+    myiAddressBook :: [Address] -> IO [Address]
+    myiAddressBook addrs = do
       putStrLn "Please select an option: 1 - Add an address, 2 - Edit an address, 3 - List all addresses, 4 - Quit"
       response <- getLine
       case response of
         "1" -> do
           addr <- inputAddressFromUser
-          myAddressBook (addr : addrs)
+          myiAddressBook (addr : addrs)
         "2" -> do
           addrs' <- editAddress addrs
-          myAddressBook addrs'
+          myiAddressBook addrs'
         "3" -> do
           printAddresses addrs
-          myAddressBook addrs
+          myiAddressBook addrs
         "4" -> do
           return addrs
         _ -> do
           putStrLn "Invalid option, please try again."
-          myAddressBook addrs
+          myiAddressBook addrs
